16522489d19a795942eb62961bad864742b19fd6
enisinanaj <sinanajeni2@outlook.com>
2022-01-25 16:44:46 +0100

    Removed test file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def convert_To_Csv():
         Function for converting txt files to csv.
     """
     read_file = pd.read_csv(r'alert_csv.txt',names=["unix_timestamp","timestamp", "dst_addr","dst_port","src_addr","src_port","proto","gid","sid","msg"], index_col=None)
+    #Change unix timestamp to DD/MM/YY HH:MM
     read_file["unix_timestamp"] =   read_file["unix_timestamp"].apply(lambda d: datetime.datetime.fromtimestamp(int(d)).strftime('%d/%m/%Y %H:%M'))
     read_file.to_csv(r'./alert.csv')
 
